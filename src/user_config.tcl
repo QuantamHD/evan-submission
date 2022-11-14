3e58ac772c70ba1eacd1c4e35584fba7b9c4b638
Ethan Mahintorabi <ethanmoon@google.com>
2022-11-14 18:11:06 +0000

    Evan's submission

--- src/user_config.tcl
+++ src/user_config.tcl
@@ -1,4 +1,4 @@
-set ::env(DESIGN_NAME) xor_shift32_quantamhd
+set ::env(DESIGN_NAME) xor_shift32_evango
 set ::env(VERILOG_FILES) "\
     $::env(DESIGN_DIR)/counter.v \
     $::env(DESIGN_DIR)/decoder.v"
